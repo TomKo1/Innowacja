c2e5960ec367956bcddd73ec11b33a7bfcc325ed
Tomasz Kot <tomek@imac-tomasz.home>
2018-09-19 17:06:46 +0200

    More config

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+app.use(function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendfile(__dirname + '/app/index.html');
+});
 app.listen(port);
